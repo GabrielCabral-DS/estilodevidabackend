7032f1ea6c8e531af85ab6e5c5740d89c3262f63
GabrielCabral-DS <cabral.jj@hotmail.com>
2025-03-11 17:58:53 -0300

    Projeto Estilo de Vida BackEnd

---
+++ src/main/java/br/com/estilodevidabackend/estilodevida/EstilodevidaApplication.java
@@ -0,0 +1,13 @@
+package br.com.estilodevidabackend.estilodevida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstilodevidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstilodevidaApplication.class, args);
+	}
+
+}
